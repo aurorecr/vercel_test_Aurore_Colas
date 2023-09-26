cf072a3875598210c75c126e764e7c0893fe6c20
aurore <38660296+aurorecr@users.noreply.github.com>
2023-09-26 20:16:31 +0100

    error quotes

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="max-w-2xl mx-auto py-10 px-4">
             <header>
-            <div className="flex items-center justify-between">
+              <div className="flex items-center justify-between">
                 <ModeToggle />
                 <nav className="ml-auto text-sm font-medium space-x-6">
                   <Link href="/">Home</Link>
@@ -37,7 +37,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
                   <Link href="/merci">Merci</Link>
                 </nav>
               </div>
-            
             </header>
             <main>{children}</main>
           </div>
